52b4b9d8769b0f54690c78ea77866efa05a74d86
Pacolele <56817740+Pacolele@users.noreply.github.com>
2024-07-28 15:33:53 +0200

    Added text to slides need to make it overlay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,9 +42,25 @@
             pagination="true"
             thumbs-swiper=".slider-thumbnail"
         >
+        
             
             <swiper-slide>
-                <img alt="" src="/images/DSC_0027.JPG">
+                <div class="overlay-container">
+                    <img alt="" src="/images/DSC_0027.JPG">
+                <div class="title" data-swiper-parallax="-300">Slide 1</div>
+                <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
+                <div class="text" data-swiper-parallax="-100">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                    dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+                    laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+                    Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+                    Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+                    ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+                    tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+                    </p>
+                </div>
+                </div>
             </swiper-slide>
             <swiper-slide>
                 <img alt="" src="/images/DSC_0056.JPG">
